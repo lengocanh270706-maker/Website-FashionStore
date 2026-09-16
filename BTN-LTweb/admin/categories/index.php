3327f01b62c8a968698f297140c3e51ee4b6c852
lengocanh270706-maker <lengocanh270706@gmail.com>
2026-09-16 10:23:44 +0700

    fix main-contain

--- BTN-LTweb/admin/categories/index.php
+++ BTN-LTweb/admin/categories/index.php
@@ -51,7 +51,7 @@ $result = $conn->query("SELECT categories.*, (SELECT COUNT(*) FROM products WHER
     </div>
 </div>
             <!-- Main Content -->
-            <div class="col-md-10 main-content">
+            <div class="col-12 col-md-9 col-lg-10 main-content">
                 <!-- Topbar góc phải chứa thông tin Admin -->
                 <div class="d-flex justify-content-between align-items-center mb-4 bg-white p-3 rounded-4 shadow-sm">
                     <h4 class="fw-bold mb-0 text-dark">Quản lý danh mục sản phẩm</h4>
